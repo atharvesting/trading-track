003adb97902db62080f80020e29732206ccede1d
generoustenacity <arawat3103@outlook.com>
2025-12-09 21:13:00 +0530

    Commit

--- analysis/backtest.py
+++ analysis/backtest.py
@@ -12,20 +12,26 @@ import pandas as pd
 # data = rel.history(start='2020-01-01', end='2025-12-01')
 # data.to_csv('src/Basic/data/reliance_data.csv')  # Save for reuse
 
-data = pd.read_csv('src/Basic/data/reliance_data.csv')
+data = pd.read_csv('analysis/reliance_data.csv')
 
 class SmaCross(Strategy):
     def init(self):
         self.sma1 = self.I(talib.SMA, self.data.Close, 20)
         self.sma2 = self.I(talib.SMA, self.data.Close, 50)
+        self.tracker = 0
         
     def next(self):
-        if crossover(self.sma1, self.sma2):
-            self.buy(size=100)
-        elif crossover(self.sma2, self.sma1):
+        if crossover(self.sma2, self.sma1):
+            self.buy(size=10)
+            self.tracker = self.data.Close[-1]
+            
+        elif crossover(self.sma1, self.sma2):
+            self.position.close()
+            
+        if self.position and self.tracker * 0.95 > self.data.Close[-1]:
             self.position.close()
 
-bt = Backtest(data, SmaCross, cash=1000000, commission=.002)
+bt = Backtest(data, SmaCross, cash=10000, commission=.002)
 stats = bt.run()
 
 print(stats)
--- analysis/nse.py
+++ analysis/nse.py
@@ -2,20 +2,12 @@ from nsetools import Nse
 import yfinance as yf
 import pandas as pd
 import numpy as np
-
-class StockAnalysis:
-    def __init__(self, data: pd.DataFrame):
-        if not isinstance(data, pd.DataFrame):
-            raise ValueError("Input must be a Pandas Dataframe")
-        self.data = data
-        
-    def get_sma(self, window: int):
-        return self.data['Close'].rolling(window=window).mean()
+from src.utils.basic import StockAnalysis
 
 ns = Nse()
 
 stock_codes = ns.get_stock_codes()
-stock_codes = stock_codes[:500]
+stock_codes = stock_codes[:25]
 
 def get_stock(stock_code: str):
     stock_id = stock_code + '.NS'
@@ -35,7 +27,3 @@ for stock in stock_codes:
     if result:
         print(stock)
         # good_list.append(result)
-        
-    
-
-    
